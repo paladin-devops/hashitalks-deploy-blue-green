fdad80c2512694808f5bc69bf3cc98c5d59844c0
Joseph Rajewski <83741749+paladin-devops@users.noreply.github.com>
2022-11-18 19:31:46 -0500

    Hello World!

--- main.go
+++ main.go
@@ -1,11 +1,7 @@
 package main
 
 import (
-	"database/sql"
-	"fmt"
 	"net/http"
-	"os"
-	"strconv"
 
 	_ "github.com/lib/pq"
 )
@@ -21,39 +17,5 @@ func main() {
 type ConnectHandler struct{}
 
 func (c ConnectHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	type conn struct {
-		user     string
-		password string
-		port     int
-		host     string
-		dbname   string
-	}
-
-	port, err := strconv.Atoi(os.Getenv("PORT"))
-	if err != nil {
-		panic(err)
-	}
-	connection := conn{
-		user:     os.Getenv("USERNAME"),
-		password: os.Getenv("PASSWORD"),
-		port:     port,
-		host:     os.Getenv("HOST"),
-		dbname:   os.Getenv("DBNAME"),
-	}
-
-	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-		"password=%s dbname=%s sslmode=disable",
-		connection.host, connection.port, connection.user, connection.password, connection.dbname)
-	db, err := sql.Open("postgres", psqlInfo)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		w.Write([]byte("Failed to connect to the database. :("))
-	} else {
-		w.Write([]byte("Connected to the database! :)"))
-	}
+	w.Write([]byte("Hello World!"))
 }
